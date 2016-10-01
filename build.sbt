95e96d3c19aba3029ab8e6f83d3318c5b4b869cb
Bernardo Uriarte <berublan@gmail.com>
2016-10-01 21:28:30 +0200

    Add routing Remove bench Update versions

--- build.sbt
+++ build.sbt
@@ -12,12 +12,10 @@ name := "Example"
 version := "0.1-SNAPSHOT"
 
 libraryDependencies ++= Seq(
-  "org.scala-js" %%% "scalajs-dom" % "0.9.0"//,
-  //"com.lihaoyi" %%% "scalatags" % "0.4.5",
-  //"com.lihaoyi" %%% "scalarx" % "0.2.7"
+  "org.scala-js" %%% "scalajs-dom" % "0.9.0",
+  "com.github.japgolly.scalajs-react" %%% "core" % "0.11.2",
+  "com.github.japgolly.scalajs-react" %%% "extra" % "0.11.2"
 )
-
-libraryDependencies += "com.github.japgolly.scalajs-react" %%% "core" % "0.11.1"
 
 bootSnippet := "example.ScalaJSExample().main()"
 
--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -1,12 +1,11 @@
 package example
 
 import japgolly.scalajs.react.vdom.all._
-import japgolly.scalajs.react.{BackendScope, ReactComponentB, ReactDOM, _}
+import japgolly.scalajs.react._
 import org.scalajs.dom.ext.KeyValue
+import japgolly.scalajs.react.extra.router._
 
 import scala.scalajs.js.annotation.JSExport
-import scalajs.js
-import org.scalajs.dom.console
 
 case class Task(id: Int, txt: String, done: Boolean) {
   override def equals(obj: scala.Any): Boolean = obj match {
@@ -15,246 +14,178 @@ case class Task(id: Int, txt: String, done: Boolean) {
   }
 }
 
-case class State(tasks: Seq[Task], editing: Option[Task], filter: String)
+sealed trait Filter
+case object All extends Filter
+case object Done extends Filter
+case object Undone extends Filter
+
+case class State(tasks: Seq[Task], editing: Option[Task])
+case class Props(filter: Filter, ctl: RouterCtl[Filter])
 
 @JSExport
 object ScalaJSExample {
 
-  val initialState = State(
-    Seq(),
-    None,
-    "All"
+  val initialState = State(Seq(), None)
+
+  val filters: Map[Filter, Task => Boolean] = Map(
+    All -> (_ => true),
+    Done -> (_.done),
+    Undone -> (!_.done)
   )
 
-  val filters: Map[String, Task => Boolean] = Map(
-    "All" -> (_ => true),
-    "Done" -> (_.done),
-    "Undone" -> (!_.done)
-  )
 
-  object react {
-
-    class Backend($: BackendScope[Unit, State]) {
-      def render(state: State) = div(
-        section(cls := "todoapp")(
-          header(cls := "header")(
-            h1("todos"),
-            input(
-              id := "new-todo",
-              cls := "new-todo",
-              placeholder := "What needs to be done?",
-              onKeyUp ==> { e: ReactKeyboardEventI =>
-                if (e.key == KeyValue.Enter && e.target.value.trim.nonEmpty) {
-                  val newId = if (state.tasks.isEmpty) 1 else state.tasks.map(_.id).max + 1
-                  val newTask = Task(newId, e.target.value.trim, false)
-                  e.target.value = ""
-                  $.setState(state.copy(tasks = newTask +: state.tasks))
-                }
-                else Callback(())
-              },
-              autoFocus := state.editing.isEmpty
-            )
-          ),
-          section(cls := "main")(
-            input(
-              id := "toggle-all",
-              cls := "toggle-all",
-              tpe := "checkbox",
-              cursor := "pointer",
-              checked := state.tasks.nonEmpty && state.tasks.forall(_.done),
-              onClick ==> { e: ReactUIEventI =>
-                $.setState(state.copy(tasks = state.tasks.map(_.copy(done = e.target.checked))))
+  class Backend($: BackendScope[Props, State]) {
+    def render(props: Props, state: State) = div(
+      section(cls := "todoapp")(
+        header(cls := "header")(
+          h1("todos"),
+          input(
+            cls := "new-todo",
+            placeholder := "What needs to be done?",
+            onKeyUp ==> { e: ReactKeyboardEventI =>
+              if (e.key == KeyValue.Enter && e.target.value.trim.nonEmpty) {
+                val newId = if (state.tasks.isEmpty) 1 else state.tasks.map(_.id).max + 1
+                val newTask = Task(newId, e.target.value.trim, false)
+                e.target.value = ""
+                $.setState(state.copy(tasks = newTask +: state.tasks))
               }
-            ),
-            label(`for` := "toggle-all", "Mark all as complete"),
-            ul(cls := "todo-list")(
-              for (task <- state.tasks if filters(state.filter)(task)) yield {
-                li(
-                  cls := {
-                    if (task.done) "completed"
-                    else if (state.editing.contains(task)) "editing"
-                    else ""
-                  },
-                  div(cls := "view")(
-                    onDblClick --> {
-                      $.setState(state.copy(editing = Some(task)))
-                    },
-                    input(
-                      cls := "toggle",
-                      tpe := "checkbox",
-                      cursor := "pointer",
-                      onChange --> {
-                        $.setState(state.copy(tasks = state.tasks.map { t =>
-                          if (t == task)
-                            t.copy(done = !t.done)
-                          else t
-                        }))
-                      },
-                      checked := task.done
-                    ),
-                    label(task.txt),
-                    button(
-                      cls := "destroy",
-                      cursor := "pointer",
-                      onClick --> {
-                        $.setState(state.copy(tasks = state.tasks.filter(_ != task)))
-                      }
-                    )
-                  ),
-                  state.editing match {
-                    case Some(editing) =>
-                      input(cls := "edit", value := editing.txt,
-                        onInput ==> { e: ReactEventI =>
-                          $.setState(state.copy(editing = state.editing.map(_.copy(txt = e.target.value.trim))))
-                        },
-                        onKeyUp ==> { e: ReactKeyboardEventI =>
-                          if (e.key == KeyValue.Enter)
-                            $.setState(state.copy(tasks = state.tasks.map { t =>
-                              if (t == editing)
-                                t.copy(txt = editing.txt)
-                              else t
-                            }, editing = None))
-                          else if (e.key == KeyValue.Escape)
-                            $.setState(state.copy(editing = None))
-                          else
-                            Callback(())
-                        },
-                        onBlur --> {
-                          $.setState(state.copy(editing = None))
-                        },
-                        autoFocus := true
-                      )
-                    case None => ""
-                  }
-                )
-              }
-            ),
-            footer(cls := "footer")(
-              span(cls := "todo-count")(strong(state.tasks.count(!_.done)), " item left"),
-              ul(cls := "filters")(
-                for ((name, pred) <- filters.toSeq) yield {
-                  li(a(
-                    cls := {
-                      if (name == state.filter) "selected"
-                      else ""
-                    },
-                    name,
-                    href := "#",
-                    onClick --> {
-                      $.setState(state.copy(filter = name))
-                    }
-                  ))
-                }
-              ),
-              button(
-                id := "clear-completed",
-                cls := "clear-completed",
-                onClick --> {
-                  $.setState(state.copy(tasks = state.tasks.filter(!_.done)))
-                },
-                "Clear completed (", state.tasks.count(_.done), ")"
-              )
-            )
+              else Callback(())
+            },
+            autoFocus := state.editing.isEmpty
           )
         ),
-        footer(cls := "info")(
-          p("Double-click to edit a todo"),
-          p(a(href := "https://github.com/lihaoyi/workbench-example-app/blob/todomvc/src/main/scala/example/ScalaJSExample.scala")("Source Code")),
-          p("Created by ", a(href := "http://github.com/lihaoyi")("Li Haoyi"))
+        section(cls := "main")(
+          input(
+            cls := "toggle-all",
+            tpe := "checkbox",
+            cursor := "pointer",
+            checked := state.tasks.nonEmpty && state.tasks.forall(_.done),
+            onClick ==> { e: ReactUIEventI =>
+              $.setState(state.copy(tasks = state.tasks.map(_.copy(done = e.target.checked))))
+            }
+          ),
+          label(`for` := "toggle-all", "Mark all as complete"),
+          ul(cls := "todo-list")(
+            for (task <- state.tasks if filters(props.filter)(task)) yield {
+              li(
+                cls := {
+                  if (task.done) "completed"
+                  else if (state.editing.contains(task)) "editing"
+                  else ""
+                },
+                div(cls := "view")(
+                  onDblClick --> {
+                    $.setState(state.copy(editing = Some(task)))
+                  },
+                  input(
+                    cls := "toggle",
+                    tpe := "checkbox",
+                    cursor := "pointer",
+                    onChange --> {
+                      $.setState(state.copy(tasks = state.tasks.map { t =>
+                        if (t == task)
+                          t.copy(done = !t.done)
+                        else t
+                      }))
+                    },
+                    checked := task.done
+                  ),
+                  label(task.txt),
+                  button(
+                    cls := "destroy",
+                    cursor := "pointer",
+                    onClick --> {
+                      $.setState(state.copy(tasks = state.tasks.filter(_ != task)))
+                    }
+                  )
+                ),
+                state.editing match {
+                  case Some(editing) =>
+                    input(cls := "edit", value := editing.txt,
+                      onInput ==> { e: ReactEventI =>
+                        $.setState(state.copy(editing = state.editing.map(_.copy(txt = e.target.value.trim))))
+                      },
+                      onKeyUp ==> { e: ReactKeyboardEventI =>
+                        if (e.key == KeyValue.Enter)
+                          $.setState(state.copy(tasks = state.tasks.map { t =>
+                            if (t == editing)
+                              t.copy(txt = editing.txt)
+                            else t
+                          }, editing = None))
+                        else if (e.key == KeyValue.Escape)
+                          $.setState(state.copy(editing = None))
+                        else
+                          Callback(())
+                      },
+                      onBlur --> {
+                        $.setState(state.copy(editing = None))
+                      },
+                      autoFocus := true
+                    )
+                  case None => ""
+                }
+              )
+            }
+          ),
+          footer(cls := "footer")(
+            span(cls := "todo-count")(strong(state.tasks.count(!_.done)), " item left"),
+            ul(cls := "filters")(
+              for ((filter, pred) <- filters.toSeq) yield {
+                li(a(
+                  cls := {
+                    if (filter == props.filter) "selected"
+                    else ""
+                  },
+                  filter.toString,
+                  href := "#",
+                  props.ctl.setOnClick(filter)
+                ))
+              }
+            ),
+            button(
+              cls := "clear-completed",
+              onClick --> {
+                $.setState(state.copy(tasks = state.tasks.filter(!_.done)))
+              },
+              "Clear completed (", state.tasks.count(_.done), ")"
+            )
+          )
         )
+      ),
+      footer(cls := "info")(
+        p("Double-click to edit a todo"),
+        p(a(href := "https://github.com/lihaoyi/workbench-example-app/blob/todomvc/src/main/scala/example/ScalaJSExample.scala")("Source Code")),
+        p("Created by ", a(href := "http://github.com/lihaoyi")("Li Haoyi"))
       )
-    }
-
-
-    val component = ReactComponentB[Unit]("TodoMVC")
-      .initialState(initialState)
-      .renderBackend[Backend]
-      .build
+    )
   }
 
 
-  def bench: Unit = {
-    import org.scalajs.dom.window._
-    import org.scalajs.dom.document._
-    import org.scalajs.dom.raw._
+  val component = ReactComponentB[Props]("TodoMVC")
+    .initialState(initialState)
+    .renderBackend[Backend]
+    .build
 
-    val enterEvent: KeyboardEvent = {
-      scalajs.js.Dynamic.newInstance(scalajs.js.Dynamic.global.KeyboardEvent)("keyup",
-        scalajs.js.Dynamic.literal(
-          "key" -> "Enter",
-          "code" -> "Enter",
-          "bubbles" -> true
-        )
-      ).asInstanceOf[KeyboardEvent]
-    }
+  val routerConfig = RouterConfigDsl[Filter].buildConfig { dsl =>
+    import dsl._
 
-    val clickEvent: MouseEvent = {
-      scalajs.js.Dynamic.newInstance(scalajs.js.Dynamic.global.MouseEvent)("click",
-        scalajs.js.Dynamic.literal("bubbles" -> true)
-      ).asInstanceOf[MouseEvent]
-    }
-
-    def add100(done: => Unit): Unit = {
-      def it(inp: HTMLInputElement, i: Int, lim: Int, done: => Unit): Unit = {
-        def enter: Unit = {
-          setTimeout(() => {
-            inp.dispatchEvent(enterEvent)
-            it(inp, i + 1, lim, done)
-          }, 0)
-        }
-        setTimeout(() => {
-          if (i < lim) {
-            inp.value = s"Todo number $i"
-            enter
-          } else done
-        }, 0)
-      }
-
-      val inp = getElementById("new-todo").asInstanceOf[HTMLInputElement]
-      it(inp, 0, 100, done)
-    }
-
-    def toggleAll(done: => Unit): Unit = {
-
-      def it(ns: NodeList, i: Int, done: => Unit): Unit = {
-        setTimeout({ () =>
-          if (i < ns.length) {
-            ns(i).dispatchEvent(clickEvent)
-            it(ns, i + 1, done)
-          }
-          else done
-        }, 0)
-      }
-      val all = querySelectorAll("input.toggle")
-      it(all, 0, done)
-    }
-
-    def clear(done: => Unit): Unit = {
-      getElementById("clear-completed").dispatchEvent(clickEvent)
-      setTimeout(done _, 0)
-    }
-
-    val t0 = performance.now()
-    add100(toggleAll(clear({
-      val t1 = performance.now()
-      alert(s"${t1 - t0}")
-    })))
+    (trimSlashes
+      | staticRoute("#all", All) ~> renderR(ctl => component(Props(All, ctl)))
+      | staticRoute("#done", Done) ~> renderR(ctl => component(Props(Done, ctl)))
+      | staticRoute("#undone", Undone) ~> renderR(ctl => component(Props(Undone, ctl)))
+      )
+      .notFound(redirectToPage(All)(Redirect.Replace))
   }
 
   @JSExport
   def main(): Unit = {
     import org.scalajs.dom.document
-    import org.scalajs.dom.raw._
     document.body.innerHTML = ""
-
-    val btnBench = document.createElement("button").asInstanceOf[HTMLButtonElement]
-    btnBench.textContent = "bench"
-    btnBench.onclick = { _: MouseEvent => bench }
-    document.body.appendChild(btnBench)
-
     val main = document.createElement("div")
     document.body.appendChild(main)
 
-    ReactDOM.render(react.component(), main)
+    val router = Router(BaseUrl.until_#, routerConfig)
+
+    ReactDOM.render(router(), main)
   }
 }
